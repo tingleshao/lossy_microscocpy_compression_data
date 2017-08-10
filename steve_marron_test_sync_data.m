32f608da4ff31b476f44d75f587aa82b4259a3d7
tingleshao <cshao@cs.unc.edu>
2017-08-09 17:10:47 -0700

    complete steve marron test code

--- steve_marron_test_sync_data.m
+++ steve_marron_test_sync_data.m
@@ -1,7 +1,7 @@
 % two tests:
 % two-distibution hypothesis test (KS test?)
 % KL divergence 
-%addpath(genpath('/playpen/cshao/lossyComp'));
+addpath(genpath('btest'));
 
 
 % read msd bead1 tracking data in 10 videos 
@@ -88,173 +88,34 @@ qqplot(test_vector_exp4,test_vector_orig)
 
 % mean test p score vs orig, compression level = 2
 % mean test p score vs orig, compression level = 15
+[h,pt2] = ttest2(test_vector_exp4_2,test_vector_orig)
+[h,pt15] = ttest15(test_vector_exp4_15,test_vector_orig)
 
-% TODO: 
 % variance-based F test p score vs orig, compression level = 2
 % variance-based F test p score vs orig, compression level = 15
+[h,pvar2] = vartest2(test_vector_exp4_2,test_vector_orig)
+[h,pvar15] = vartest2(test_vector_exp4_15,test_vector_orig)
 
 % nscore test, compression level = 2
 % nscore test, compression level = 15
+input_data2 = [test_vector_exp4_2'; test_vector_orig'];
+stats2 = nscores(input_data2)
+
+input_data15 = [test_vector_exp4_15'; test_vector_orig'];
+stats15 = nscores(input_data15)
 
 % TOST vs orig, compression level = 2
 % TOST vs orig, compression level = 15
+tostp1_2, tostp2_2, tostci_2 = TOST(test_vector_exp4_2, test_vector_orig, -0.01, 0.01, 0.05)
+tostp1_15, tostp2_15, tostci_15 = TOST(test_vector_exp4_15, test_vector_orig, -0.01, 0.01, 0.05)
+
 
 % kernel density estimation, compression level = 2
 % kernel density estimation, compression level =  15
-
-%%%%%%%%%%%%%%%%%% statistical tests %%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%% statistical tests %%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%% statistical tests %%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%% statistical tests %%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%% statistical tests %%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%% statistical tests %%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%% statistical tests %%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%% statistical tests %%%%%%%%%%%%%%%%%%
-
-% % plot and fit the distribution of one (qp) 10 video 1 bead msd 
-% tau_index = 3;
-% qp_index = 2;
-% 
-% msd_data_for_plot = exp_01_msd_data(tau_index, :, qp_index);
-% orig_msd_data_for_plot = orig_exp_msd(tau_index, :);
-% 
-% figure;
-% hold on 
-% histdata = [msd_data_for_plot', orig_msd_data_for_plot'];
-% hist(histdata);
-% hold off 
-% 
-% % try a hist plot for exp_04 
-% tau_index = 3;
-% dilation_index = 15; 
-% msd_data_for_plot = exp_04_msd_data(tau_index, :, dilation_index);
-% 
-% figure;
-% hold on 
-% histdata = [msd_data_for_plot', orig_msd_data_for_plot'];
-% size(histdata)
-% hist(histdata);
-% hold off 
-
-if 1
-% generate the ks test result table 
-% exp_01
-tau_index = 3;
-[exp_01_ks_test_h, exp_01_ks_test_p] = compute_ks_test_row(orig_exp_msd, exp_01_msd_data, tau_index, exp_01_qp_list);
-
-% exp_02
-[exp_02_ks_test_h, exp_02_ks_test_p] = compute_ks_test_row(orig_exp_msd, exp_02_msd_data, tau_index, exp_02_qp_list);
-
-% exp_03
-[exp_03_ks_test_h, exp_03_ks_test_p] = compute_ks_test_row(orig_exp_msd, exp_03_msd_data, tau_index, exp_03_dilation_list);
-
-% exp_04 
-[exp_04_ks_test_h, exp_04_ks_test_p] = compute_ks_test_row(orig_exp_msd, exp_04_msd_data, tau_index, exp_04_dilation_list);
-
-% exp_05
-[exp_05_ks_test_h, exp_05_ks_test_p] = compute_ks_test_row(orig_exp_msd, exp_05_msd_data, tau_index, exp_05_qp_list);
-
-%figure
-%%hold on 
-%plot(exp_01_avg_file_compression_ratios, exp_01_ks_test_h, '-.k*',exp_02_avg_file_compression_ratios, exp_02_ks_test_h, '--ko', exp_03_avg_file_compression_ratios, exp_03_ks_test_h, ':ks',exp_03_avg_file_compression_ratios, exp_04_ks_test_h, '-kx',exp_01_avg_file_compression_ratios, exp_05_ks_test_h, '->k')
-%set(gca, 'XScale', 'log')
-
-%hold off
-%figure
-%hold on 
-%%plot(exp_01_avg_file_compression_ratios, exp_01_ks_test_p, '-.k*', exp_02_avg_file_compression_ratios, exp_02_ks_test_p, '--ko', exp_03_avg_file_compression_ratios, exp_03_ks_test_p, ':ks', exp_03_avg_file_compression_ratios, exp_04_ks_test_p, '-kx', exp_01_avg_file_compression_ratios, exp_05_ks_test_p, '->k', exp_02_avg_file_compression_ratios, ones(size(exp_02_avg_file_compression_ratios))*0.05, '-r')
-%legend('custom ffmpeg (variation 2)', 'standard ffmpeg', 'dilation (variation 1)', 'dilation with post-processing (variation 1)', 'custom ffmpeg post-processing (variation 2)', 'Location', 'northeast');
-%set(gca, 'XScale', 'log')
-
-%hold off 
-%xlabel('file size in bytes');
-%ylabel('KS test p scores between compressed video MSD distributions and original video MSD distributions');
+[h, pbtest2] = btest(test_vector_exp4_2, test_vector_exp4_2)
+[h, pbtest15] = btest(test_vector_exp4_15, test_vector_orig)
 
 
 
-% generate the KL divergence result table 
-% exp_01 
-exp_01_kldiv_test_values = compute_kldiv_test_row(orig_exp_msd, exp_01_msd_data, tau_index, exp_01_qp_list); 
 
-% exp_02 
-exp_02_kldiv_test_values = compute_kldiv_test_row(orig_exp_msd, exp_02_msd_data, tau_index, exp_02_qp_list);
 
-% exp_03
-exp_03_kldiv_test_values = compute_kldiv_test_row(orig_exp_msd, exp_03_msd_data, tau_index, exp_03_dilation_list);
-
-% exp_04
-exp_04_kldiv_test_values = compute_kldiv_test_row(orig_exp_msd, exp_04_msd_data, tau_index, exp_04_dilation_list);
-exp_05_kldiv_test_values = compute_kldiv_test_row(orig_exp_msd, exp_05_msd_data, tau_index, exp_05_qp_list);
-
-%figure
-%hold on 
-%plot(exp_01_avg_file_compression_ratios, exp_01_kldiv_test_values, '-.k*',exp_02_avg_file_compression_ratios, exp_02_kldiv_test_values, '--ko', exp_03_avg_file_compression_ratios, exp_03_kldiv_test_values, ':ks',exp_03_avg_file_compression_ratios, exp_04_kldiv_test_values, '-kx',exp_01_avg_file_compression_ratios, exp_05_kldiv_test_values, '->k')
-%set(gca, 'YScale', 'log')
-%set(gca, 'YScale', 'log')
-%set(gca, 'XScale', 'log')
-
-%legend('custom ffmpeg (variation 2)', 'standard ffmpeg', 'dilation (variation 1)', 'dilation with post-processing (variation 1)', 'custom ffmpeg post-processing (variation 2)', 'Location', 'northeast');
-%hold off 
-%xlabel('file size in bytes');
-%ylabel('KL divergence values between compressed video MSD distributions and original video MSD distributions');
-
-% plot single comparison plots 
-figure
-hold on 
-%plot(exp_02_avg_file_compression_ratios, exp_02_ks_test_p, '--ro', exp_01_avg_file_compression_ratios, exp_05_ks_test_p, '->b', exp_02_avg_file_compression_ratios, ones(size(exp_02_avg_file_compression_ratios))*0.05, '-r' , [18.4701, 18.4701], [0, 1], '-b', [h264_data_ratio, h264_data_ratio], [0,1], '-r')
-plot(exp_02_avg_file_compression_ratios, exp_02_ks_test_p, '-o', 'Color', [124/255, 70/255, 34/255], 'LineWidth', 3, 'MarkerSize', 8)
-plot(exp_03_avg_file_compression_ratios, exp_03_ks_test_p, '-*', 'Color', [44/255, 52/255, 162/255],  'LineWidth', 3, 'MarkerSize', 8)
-plot(exp_03_avg_file_compression_ratios, exp_04_ks_test_p, '-.', 'Color', [44/255, 139/255, 162/255], 'LineWidth', 3, 'MarkerSize', 20)
-plot(exp_01_avg_file_compression_ratios, exp_01_ks_test_p, '-x', 'Color', [34/255, 124/255, 40/255], 'LineWidth', 3, 'MarkerSize', 12)
-plot(exp_01_avg_file_compression_ratios, exp_05_ks_test_p, '->', 'Color', [53/255, 201/255, 16/255],  'LineWidth', 3, 'MarkerSize', 8)
-%plot(5:100:10000, ones([100,1])*0.05, '-k' ,'LineWidth', 3, 'MarkerSize', 8)
-%plot(5:100:10000, ones([100,1])*0.1, '-k' ,'LineWidth', 3, 'MarkerSize', 8)
-%plot(5:100:10000, ones([100,1])*0.5, '-k' ,'LineWidth', 3, 'MarkerSize', 8)
-%plot(5:100:10000, ones([100,1])*0.9, '-k' ,'LineWidth', 3, 'MarkerSize', 8)
-plot(5:100:10000, ones([100,1])*0.95, '-k' ,'LineWidth', 3, 'MarkerSize', 8)
-plot([18.4701, 18.4701], [10^(-10), 10], '-k','LineWidth', 3, 'MarkerSize', 8)
-
-%plot([18.4701, 18.4701], [0, 1], '-k','LineWidth', 3, 'MarkerSize', 8)
-%plot([32.9, 32.9], [0, 1], '-k','LineWidth', 3, 'MarkerSize', 8)
-%plot([39.2, 39.2], [0, 1], '-k','LineWidth', 3, 'MarkerSize', 8)
-
-%plot([178, 178], [0, 1], '-k','LineWidth', 3, 'MarkerSize', 8)
-%plot([149, 149], [0, 1], '-k','LineWidth', 3, 'MarkerSize', 8)
-%plot([55, 55], [0, 1], '-k','LineWidth', 3, 'MarkerSize', 8)
-%plot([41, 41], [0, 1], '-k','LineWidth', 3, 'MarkerSize', 8)
-
-axis([10, 10000, 0, 1])
-h = legend('H.264', 'Analysis-Aware (V1)', 'Analysis-Aware + Post Processing (V1)',  'Analysis-Aware (V2)', 'Analysis-Aware + Post Processing (V2)','Location', 'northeast');
-set(h,'FontSize',14);
-set(gca, 'XScale', 'log', 'FontSize', 14)
-hold off 
-xlabel('compression ratio');
-ylabel('KS test p score');
-
-figure
-hold on 
-
-plot(exp_02_avg_file_compression_ratios, exp_02_kldiv_test_values, '-o', 'Color', [124/255, 70/255, 34/255], 'LineWidth', 3, 'MarkerSize', 8)
-plot(exp_03_avg_file_compression_ratios, exp_03_kldiv_test_values, '-*', 'Color', [44/255, 52/255, 162/255],  'LineWidth', 3, 'MarkerSize', 8)
-plot(exp_03_avg_file_compression_ratios, exp_04_kldiv_test_values, '-.', 'Color', [44/255, 139/255, 162/255], 'LineWidth', 3, 'MarkerSize', 20)
-plot(exp_01_avg_file_compression_ratios, exp_01_kldiv_test_values, '-x', 'Color', [34/255, 124/255, 40/255], 'LineWidth', 3, 'MarkerSize', 12)
-plot(exp_01_avg_file_compression_ratios, exp_05_kldiv_test_values, '->', 'Color', [53/255, 201/255, 16/255],  'LineWidth', 3, 'MarkerSize', 8)
-
-%plot(exp_02_avg_file_compression_ratios, exp_02_kldiv_test_values, '-ro','LineWidth', 3, 'MarkerSize', 8)
-%%plot(exp_03_avg_file_compression_ratios, exp_03_kldiv_test_values, '->b', 'LineWidth', 3, 'MarkerSize', 8)
-%plot(exp_03_avg_file_compression_ratios, exp_04_kldiv_test_values, '->g', 'LineWidth', 3, 'MarkerSize', 8)
-%plot(exp_01_avg_file_compression_ratios, exp_01_kldiv_test_values, '->y', 'LineWidth', 3, 'MarkerSize', 8)
-%plot(exp_01_avg_file_compression_ratios, exp_05_kldiv_test_values, '->k', 'LineWidth', 3, 'MarkerSize', 8)
-plot([18.4701, 18.4701], [10^(-10), 10], '-k','LineWidth', 3, 'MarkerSize', 8)
-axis([10, 10000, 10^(-10), 10])
-plot([1450, 1450], [10^(-10), 10], '-k','LineWidth', 3, 'MarkerSize', 8)
-
-set(gca, 'YScale', 'log')
-set(gca, 'XScale', 'log', 'FontSize', 14)
-
-h = legend('H.264', 'Analysis-Aware (V1)', 'Analysis-Aware + Post Processing (V1)',  'Analysis-Aware (V2)', 'Analysis-Aware + Post Processing (V2)','Location', 'northeast');
-set(h,'FontSize',14);
-hold off 
-xlabel('compression ratio');
-ylabel('KL divergence value');
-end
